rename enviornments, add expectation game, implement observations, fix bug in reward calculation

simple_map.py
@@ -11,7 +11,9 @@ class SimpleMap(gym.Env):
         "px_scale": 5,
     }
 
-    def __init__(self, num_agent: int, num_target: int, map: np.ndarray, prior: np.ndarray, render_mode: Optional[str] = None, max_iter=None):
+    def __init__(self, num_agent: int, num_target: int,
+                 map: np.ndarray, prior: np.ndarray,
+                 render_mode: Optional[str] = None, max_iter=None):
         """Construction for SimpleMap environment
 
         Args:
@@ -35,9 +37,11 @@ class SimpleMap(gym.Env):
 
         self.targets: List[util.Target] = []
         self.agents: List[util.Agent] = []
+        self.agent_trajectories = np.zeros((*map.shape, num_agent))
         self.iter = 0
 
-        self.observation_space = gym.spaces.Box(low=np.zeros(prior.shape), high=np.ones(prior.shape))
+        obs_shape = (num_agent, *prior.shape, 6)
+        self.observation_space = gym.spaces.Box(low=np.zeros(obs_shape), high=np.ones(obs_shape))
         # 5 movements: N/W/S/E/none; 2 sensing options: on/off
         self.action_space = gym.spaces.Tuple([
             gym.spaces.Tuple((gym.spaces.Discrete(5), gym.spaces.Discrete(2))) for _ in range(self.num_agent)
@@ -64,20 +68,51 @@ class SimpleMap(gym.Env):
         reward = 0
 
         # Move all agents
-        for agent, (mov, sense) in zip(self.agents, action):
+        for i, (agent, (mov, sense)) in enumerate(zip(self.agents, action)):
             dx, dy = motions[mov]
             agent.move(dx, dy)
 
             if sense:
                 reward -= self.sensing_cost
                 reward += agent.sense(self.targets)
+                xi, yi = agent.get_position()
+                self.agent_trajectories[xi, yi, i] += 1
 
         self.iter += 1
         trunc = (self.max_iter is not None) and (self.iter > self.max_iter)
 
         # termination condition
         terminate = not np.any([targ.active for targ in self.targets])
-        return (self.map, reward, terminate, trunc, {})
+
+        agent_locs_map = np.zeros(self.map.shape)
+        for i in range(self.num_agent):
+            xi, yi = self.agents[i].get_position()
+            agent_locs_map[xi, yi] = 1
+
+        # observation channels:
+        #   0: prior distr
+        #   1: map & walls
+        #   2: current position
+        #   3: neighbors
+        #   4: previous trajectory
+        #   5: neighbor previous trajectory
+
+        observations = []
+        for i in range(self.num_agent):
+            xi, yi = self.agents[i].get_position()
+            observation = np.zeros((*self.map.shape, 6))
+            observation[..., 0] = self.prior
+            observation[..., 1] = self.map
+            observation[xi, yi, 2] = 1
+            observation[..., 3] = agent_locs_map - observation[..., 2]
+            observation[..., 4] = self.agent_trajectories[..., i]
+            observation[..., 4] = observation[..., 4] / np.sum(observation[..., 4])
+            observation[..., 5] = np.sum(self.agent_trajectories, axis=-1) - self.agent_trajectories[..., i]
+            observation[..., 5] = observation[..., 5] / np.sum(observation[..., 5])
+
+            observations.append(observation)
+
+        return (np.array(observations), reward, terminate, trunc, {})
 
     def reset(self):
         self.objects = []
@@ -181,15 +216,32 @@ if __name__ == '__main__':
         prior += np.exp(-((xx - cx)**2 + (yy-cy)**2) / 2/sig/sig)
     prior[map != 0] = 0
 
-    env = SimpleMap(1, 50, map, prior, render_mode='human')
-    aa = util.Agent('r0', (100, 100), env.map, env.metadata['px_scale'])
-    env.set_agents([aa])
+    env = SimpleMap(3, 50, map, prior, render_mode='human')
+    a1 = util.Agent('r0', (100, 100), env.map, env.metadata['px_scale'])
+    a2 = util.Agent('r1', (50, 150), env.map, env.metadata['px_scale'])
+    a3 = util.Agent('r3', (150, 50), env.map, env.metadata['px_scale'])
+    env.set_agents([a1, a2, a3])
     env.reset()
-    env.render()
+    # env.render()
 
-    import random
-    while True:
-        dr = [(random.randint(0, 3), 1)]
-        for _ in range(3):
-            env.step(dr)
-        env.render()
+    # traj = [(0, 1), (1, 1), (3, 1)]
+
+    # import random
+
+    rr = []
+    import tqdm
+    for _ in (pbar := tqdm.tqdm(range(1000))):
+        tot_reward = 0
+        env.reset()
+        for _ in range(90):
+            # dr = [(random.randint(0, 3), 1), (random.randint(0, 3), 1), (random.randint(0, 3), 1)]
+            dr = [(0, 1), (3, 1), (1, 1)]
+            # dr = [(4, 1), (4, 1), (4, 1)]
+            # for _ in range(3):
+            obs, reward, term, trunc, info = env.step(dr)
+            tot_reward += reward
+            # env.render()
+        rr.append(tot_reward)
+        pbar.set_description(f'mean reward: {np.mean(rr):.03f}; std: {np.std(rr) / np.sqrt(len(rr)):.03f}')
+    print(f'mean reward: {np.mean(rr):.03f}; std: {np.std(rr) / np.sqrt(len(rr)):.03f}')
+    print(rr)

util.py
@@ -101,7 +101,7 @@ class Target(MapObject):
 
 
 class Agent(MapObject):
-    def __init__(self, name: str, x0: Tuple[int], map: np.ndarray, scale: int):
+    def __init__(self, name: str, x0: Tuple[int], map: np.ndarray, scale: int=0):
         super(Agent, self).__init__(name, x0, map, scale)
 
         self.ksize = 25
@@ -129,6 +129,8 @@ class Agent(MapObject):
     def sense(self, targets: List[Target]):
         n_collect = 0
         for targ in targets:
+            if not targ.active:
+                continue
             rel_pos = targ.get_position() - self.get_position()
 
             if np.any(np.abs(rel_pos) > self.ksize // 2):